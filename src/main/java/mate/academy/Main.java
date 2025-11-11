f6dc779172558c5175a53a840ee7876ca7cc5dd4
ivankosh <ivan2023koshovyi@gmail.com>
2025-11-11 18:47:40 +0200

    Implemented CinemaHallDaoImpl and CinemaHallServiceImpl.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import mate.academy.lib.Inject;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
     public static void main(String[] args) {
-        MovieService movieService = null;
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHallService cinemaHallService =
+                (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        CinemaHall hallOne = new CinemaHall();
+        hallOne.setCapacity(30);
+        hallOne.setDescription("VR-Hall");
+        cinemaHallService.add(hallOne);
+        System.out.println(cinemaHallService.get(hallOne.getId()));
+
+
     }
 }
